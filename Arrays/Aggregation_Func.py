8f31a982cf75f0c3c208d1c1cec2c3a4827142e5
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-01 18:05:57 +0530

    s

---
+++ Arrays/Aggregation_Func.py
@@ -0,0 +1,14 @@
+# Aggregation Function : Means To Get Summarary From Data
+
+import numpy as np 
+
+lst = [10,20,30]
+
+a = np.array([lst])
+print(np.sum(a)) # Sum Of All Array Elements
+print(np.mean(a))# Mean Of All Array Elements
+print(np.min(a))# Minimum Of All Array Elements
+print(np.max(a))# Maxmium Of All Array Elements
+print(np.std(a))# Standard Deviation Of All Array Elements
+print(np.var(a))# Variance Of All Array Elements
+

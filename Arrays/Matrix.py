d8e6fab9afb6376914e02b35db44a87b3bf113bb
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-01 12:13:52 +0530

    d

---
+++ Arrays/Matrix.py
@@ -0,0 +1,12 @@
+# Matrix can be created atleast 2D Array we can fill them with zeores or ones or any specific numbers
+
+import numpy as np
+
+# arr = np.zeros((3,4)) # For Filling Up With All Zerores
+# arr = np.ones((3,4)) # For Filling Up With All Ones
+# arr = np.full((3,4),8) # For Filling Up With All specific number
+# arr = np.eye((3)) # For Creating A diangonal in a matrices
+arr = np.arange(1,10,2) # For Creating a Step Up Array
+
+
+print(arr)

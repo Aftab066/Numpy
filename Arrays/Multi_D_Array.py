c2d8ce86ada159c875c2e578462ef5192c176296
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-01 11:57:09 +0530

    u

---
+++ Arrays/Multi_D_Array.py
@@ -0,0 +1,11 @@
+#Multi D :- It Contains More Than 2d Array 
+
+import numpy as np
+
+arr = np.array([[1,2,3],
+               [4,5,6],
+               [7,8,9],
+               [10,11,12],
+               [13,14,15]])
+
+print(arr)

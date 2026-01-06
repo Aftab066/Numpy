5dd3cb0782f022ed766bb81f3c1727d232c142f7
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-06 19:48:51 +0530

    a

--- Arrays/Arrays_Properties.py
+++ Arrays/Arrays_Properties.py
@@ -3,7 +3,7 @@
 import numpy as np 
 
 a = np.array([1,2,3])
-# print(a.shape) #To Know Shape
+print(a.shape) #To Know Shape
 # print(a.size) #To Know size
 # print(a.ndim) #To Know No Of Dimensions
 # print(a.dtype) #To Know Data Type Of Array Elements

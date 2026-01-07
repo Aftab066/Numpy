6d216d5d7f72caa5645282c83a01bf4da05e8d34
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-07 20:51:50 +0530

    l

--- Arrays/Reshaping_Array.py
+++ Arrays/Reshaping_Array.py
@@ -17,10 +17,11 @@ reshaped = arr.reshape(2,4)
 arr_2 = np.array([[1,2,3,4],
                   [5,6,7,8]])
     
-# ay = arr_2.ravel()
-# print(arr_2)
-# print(ay)
 
-ay1 = arr_2.flatten()
-print(ay1)
+ay = arr_2.ravel()
 print(arr_2)
+print(ay)
+
+# ay1 = arr_2.flatten()
+# print(ay1)
+# print(arr_2)

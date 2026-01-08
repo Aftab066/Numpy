37cb7ac941c77fabfbd666962a76e84dce92a11e
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-08 21:52:53 +0530

    a

---
+++ Advanced Numpy/Manipluating_Array.py
@@ -0,0 +1,20 @@
+#Insert Keyword at Specific Position in Numpy Array
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6,7,8])
+
+# new_arr = np.insert(arr, 4, 99) # 4 is the index position where 99 will be inserted
+
+# print("Original Array:", arr)
+# print("Array after Insertion:", new_arr)
+
+#Append Element to Numpy Array
+
+# arr_appended = np.append(arr, [10, 11, 12])
+# print("Original Array:", arr)
+# print("Array after Appending:", arr_appended)
+
+# Note: Both np.insert and np.append return a new array and do not modify the original array.
+
+
